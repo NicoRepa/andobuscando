a8a1d1ff31d10702c899caa3ee22e06d8de14f1f
NicoRepa <112137882+NicoRepa@users.noreply.github.com>
2025-05-25 22:28:36 -0300

    Update settings.py

--- andoBuscando/settings.py
+++ andoBuscando/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorag"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
